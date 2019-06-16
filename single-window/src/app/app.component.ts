d69efb278255db7361fee9a12b7660593e5252e8
Yasunobu Kawaguchi <kawaguti@gmail.com>
2019-06-16 17:25:25 +0900

    Add clock
    https://qiita.com/karintou/items/04a279a92f2055ee07a1

--- single-window/src/app/app.component.ts
+++ single-window/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'single-window';
+
+  now: Observable<Date>;
+  intervalList = [];
+
+  constructor() {
+  }
+
+  public ngOnInit() {
+    this.now = new Observable((observer) => {
+      this.intervalList.push(setInterval(() => {
+        observer.next(new Date());
+      }, 1000));
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.intervalList) {
+      this.intervalList.forEach((interval) => {
+        clearInterval(interval);
+      });
+    }
+  }
 }
