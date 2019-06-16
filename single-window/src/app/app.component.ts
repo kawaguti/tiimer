e5c8253ff091cebf1c86e220b7a9b11a24f969f1
Yasunobu Kawaguchi <kawaguti@gmail.com>
2019-06-16 17:59:28 +0900

    Add countdown timer

--- single-window/src/app/app.component.ts
+++ single-window/src/app/app.component.ts
@@ -11,12 +11,31 @@ export class AppComponent implements OnInit, OnDestroy {
   now: Observable<Date>;
   intervalList = [];
 
+  targetSec: number;
+  elapsedSec: number;
+  remainedSec: number;
+  remainedString: string;
+
   constructor() {
   }
 
   public ngOnInit() {
+    this.remainedString = '00:00';
+    const newsec = 180;
+    if ( newsec > 0 && newsec < 60 * 60 ) {
+      this.targetSec = newsec;
+      this.elapsedSec = 0;
+      this.remainedSec = newsec;
+    }
     this.now = new Observable((observer) => {
       this.intervalList.push(setInterval(() => {
+        if ( this.targetSec > 0 && this.remainedSec > 0) {
+          this.elapsedSec++;
+          this.remainedSec = this.targetSec - this.elapsedSec;
+          this.remainedString =
+            Math.floor(this.remainedSec / 60).toString().padStart(2, '0') + ':' +
+            Math.floor(this.remainedSec % 60).toString().padStart(2, '0');
+        }
         observer.next(new Date());
       }, 1000));
     });
